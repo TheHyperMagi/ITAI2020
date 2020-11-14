7dee6c1ac48d1c99e39fdc1c30a3a23fb002d4af
thehypermagi <shamil.haqeem@gmail.com>
2020-11-14 17:36:23 +0800

    Deleted irrelevant files

--- README.md
+++
@@ -1,3 +0,0 @@
-# Intro to AI Semester 2, 2020
-- Subject: TBD
-- Data Source: TBD
--- tensorflow_implementation.py
+++ tensorflow_implementation.py
@@ -45,18 +45,16 @@ first_image = image_batch[0]
 # Notice the pixels values are now in `[0,1]`.
 print(np.min(first_image), np.max(first_image)) 
 
-num_classes = 12
+num_classes = 4
 
 model = Sequential([
-  data_augmentation,
-  layers.experimental.preprocessing.Rescaling(1./255),
+  layers.experimental.preprocessing.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
   layers.Conv2D(16, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
   layers.Conv2D(32, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
   layers.Conv2D(64, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
-  layers.Dropout(0.2),
   layers.Flatten(),
   layers.Dense(128, activation='relu'),
   layers.Dense(num_classes)
